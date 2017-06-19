e26a3ea8439e38d3c30ad46ef0c0073a51082962
Loquat Solutions <stock@loquatsolutions.com>
2017-06-19 12:13:52 +0100

    primer commit

--- TaskManajer/ViewController.swift
+++ TaskManajer/ViewController.swift
@@ -8,11 +8,51 @@
 
 import UIKit
 
+import Alamofire
+import SwiftyJSON
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        // FUNCION GET
+        Alamofire.request("http://www.loquatsolutions.com/detres/detres/api/mostrarPedidos").responseData { (resData) -> Void in
+            print(resData.result.value!)
+            let strOutput = String(data : resData.result.value!, encoding : String.Encoding.utf8)
+            print(strOutput!)
+        }
+       
+        
+        
+        
+       
+        // FUNCION POST
+    
+        let urlstring = "http://www.loquatsolutions.com/detres/detres/api/pruebaAyo"
+       
+        // AQUI PONES LOS PARAMETROS QUE TU QUIERAS ESTOS NO SIRVEN DE NADA
+        let parameters:  [String: Any] = [
+            "client_id" : "xxxxxx",
+            "client_secret" : "xxxxx",
+            "device_token" : "xxxx",
+            "fullname" : "xxxxx",
+            "gender": "xxx"
+        ]
+        
+        
+     
+    
+        Alamofire.request(urlstring, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).responseJSON { response in
+            print(response)
+        }
+        
+        
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
